b2a30af9c832e277c8e728e48f7da474cd9e6607
Tyrthurey <tyrthurey@gmail.com>
2026-04-18 13:44:57 +0300

    Fix event bus registration and update CI workflow
    - Register `FarsightedMobsNeoForge` to NeoForge event bus
    - Update `mc-publish` action to v3.3 and configure permissions in release workflow

--- neoforge/src/main/java/net/sindarin27/farsightedmobs/neoforge/FarsightedMobsNeoForge.java
+++ neoforge/src/main/java/net/sindarin27/farsightedmobs/neoforge/FarsightedMobsNeoForge.java
@@ -1,7 +1,6 @@
 package net.sindarin27.farsightedmobs.neoforge;
 
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 
 import net.neoforged.neoforge.common.NeoForge;
@@ -10,13 +9,13 @@ import net.neoforged.neoforge.event.server.ServerAboutToStartEvent;
 import net.sindarin27.farsightedmobs.FarsightedMobs;
 
 @Mod(FarsightedMobs.MOD_ID)
-@EventBusSubscriber(modid = FarsightedMobs.MOD_ID)
 public final class FarsightedMobsNeoForge {
     public FarsightedMobsNeoForge() {
         // Run our common setup.
         FarsightedMobs.init();
 
         NeoForge.EVENT_BUS.addListener((ServerAboutToStartEvent e) -> FarsightedMobs.attributeRulesManager.setRegistryLookup(e.getServer().registryAccess()));
+        NeoForge.EVENT_BUS.register(FarsightedMobsNeoForge.class);
     }
 
     @SubscribeEvent
